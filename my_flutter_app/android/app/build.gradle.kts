fddc62928def9e8530db7e16e4d498180cd078ec
Vietbach-100299 <leevietbach0610@gmail.com>
2025-07-03 16:37:00 +0700

    add github

--- my_flutter_app/android/app/build.gradle.kts
+++ my_flutter_app/android/app/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 android {
     namespace = "com.example.my_flutter_app"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    ndkVersion = "27.0.12077973"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
@@ -16,7 +16,7 @@ android {
     }
 
     kotlinOptions {
-        jvmTarget = JavaVersion.VERSION_11.toString()
+        jvmTarget = "17"
     }
 
     defaultConfig {
